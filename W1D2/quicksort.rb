878a794547ea208f8f6b9c930d5238b512bd058f
Aaron <astrauli@gmail.com>
2017-10-03 20:34:03 -0700

    added working quicksort

---
+++ W1D2/quicksort.rb
@@ -0,0 +1,21 @@
+#extra_practice
+
+def quicksort(array)
+  return array if array.length == 1 || array.length == 0
+  pivot_el = array[0]
+  left_side = []
+  array.each do |el|
+    left_side << el if el < pivot_el
+  end
+  right_side = []
+  array.each do |el|
+    right_side << el if el > pivot_el
+  end
+  # p left_side + [pivot_el]
+  # p pivot_el
+  # p right_side
+  # p left_side
+  quicksort(left_side) + [pivot_el] + quicksort(right_side)
+end
+
+p quicksort([7,1,8,3,5,9,11,2,4])
